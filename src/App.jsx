323306f086263bb0a3f2e1787a0f48ef4da4bb73
Andreas-206 <puma_real@ukr.net>
2024-02-18 10:51:02 +0200

    Description

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,56 @@
 import { useState, useEffect } from 'react'
-import Description from "./components/Description/Description"
-import Feedback from "./components/Feedback/Feedback"
-import Options from "./components/Options/Options"
+import Description from './components/Description/Description'
+import Feedback from './components/Feedback/Feedback'
+import Options from './components/Options/Options'
 import Notification from './components/Notification/Notification'
 import './App.css'
 
 const App = ({}) => {
-  const initialState = JSON.parse(localStorage.getItem("feedbackTypes")) || {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+	const initialState = JSON.parse(localStorage.getItem('feedbackTypes')) || {
+		good: 0,
+		neutral: 0,
+		bad: 0,
+	}
 
-  const [feedbackTypes, setFeedbackTypes] = useState(initialState);
+	const [feedbackTypes, setFeedbackTypes] = useState(initialState)
 
-  useEffect(() => {
-    localStorage.setItem("feedbackTypes", JSON.stringify(feedbackTypes));
-  }, [feedbackTypes]);
+	useEffect(() => {
+		localStorage.setItem('feedbackTypes', JSON.stringify(feedbackTypes))
+	}, [feedbackTypes])
 
-  const updateFeedback = feedbackType => {
-    setFeedbackTypes((prevFeedback) => ({
-      ...prevFeedback,
-      [feedbackType]: prevFeedback[feedbackType] + 1,
-    }));
-  };
+	const updateFeedback = feedbackType => {
+		setFeedbackTypes(prevFeedback => ({
+			...prevFeedback,
+			[feedbackType]: prevFeedback[feedbackType] + 1,
+		}))
+	}
 
-  const totalFeedback = feedbackTypes.good + feedbackTypes.neutral + feedbackTypes.bad;
+	const totalFeedback =
+		feedbackTypes.good + feedbackTypes.neutral + feedbackTypes.bad
 
-  const positivePercentage = totalFeedback > 0 ? Math.round(((feedbackTypes.good + feedbackTypes.neutral) / totalFeedback) * 100) : 0;
+	const positivePercentage =
+		totalFeedback > 0
+			? Math.round(
+					((feedbackTypes.good + feedbackTypes.neutral) / totalFeedback) * 100) : 0
 
-
-   return (
-    <>
-      <Description />
-      <Options onLeaveFeedback={updateFeedback} totalFeedback={totalFeedback} />
-      <Notification />
-      {totalFeedback > 0 ? (
-        <Feedback
-          feedbackTypes={feedbackTypes}
-          totalFeedback={totalFeedback}
-          positivePercentage={positivePercentage}
-        />
-      ) : (
-        null 
-      )}
-    </>
-  )
+	return (
+		<>
+			<Description
+				title='Sip Happens Café'
+				content='Please leave your feedback about our service by selecting one of the
+				options below.'
+			/>
+			<Options onLeaveFeedback={updateFeedback} totalFeedback={totalFeedback} />
+			<Notification />
+			{totalFeedback > 0 ? (
+				<Feedback
+					feedbackTypes={feedbackTypes}
+					totalFeedback={totalFeedback}
+					positivePercentage={positivePercentage}
+				/>
+			) : null}
+		</>
+	)
 }
 
-
-
-export default App;
+export default App
